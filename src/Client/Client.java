6c3b889a6ba7ec6094105da3edf31531e3036905
Ezra Howe <howe.ez@northeastern.edu>
2024-05-16 14:56:32 -0400

    new client

---
+++ src/Client/Client.java
@@ -0,0 +1,10 @@
+package Client;
+
+public class Client {
+
+    public void initialize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+    
+}
