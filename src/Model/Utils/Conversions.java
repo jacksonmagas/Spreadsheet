1b4f48037cd7b6475b683b2e93bec734c565cda4
Jackson <jacksonmagas@gmail.com>
2024-05-16 21:43:51 -0400

    Add conversions class Converts from reference to row/col

--- src/Model/Utils/Conversions.java
+++ src/Model/Utils/Conversions.java
@@ -1,23 +1,70 @@
-public final class Conversions {
-    static String REF_FORMAT = "^\\$\\w*\\d*$";
+package Model.Utils;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public final class Conversions {
+
+    /**
+     * Check if the string is a reference of the proper form
+     * @param ref the string reference to a cell in the form "$A1" for first column first row
+     * @return True if the string is a reference of the proper form
+     */
     static boolean isValidRef(String ref) {
-        return ref.matches("^\\$\\w*\\d*$");
+        Pattern validRef = Pattern.compile("^\\$\\[a-zA-Z]+\\d+$");
+        return validRef.matcher(ref).matches();
     }
 
-    /** get the column number from the cell reference like "$A1" 
-     * 
+
+    /**
+     * Get the row number of the reference string.
+     * @param reference the string reference to a cell in the form "$A1" for first column first row
+     * @throws IllegalArgumentException if the string is not a reference
+     * @return The 1 indexed row number
+     */
+    static int row(String reference) {
+        if (!isValidRef(reference)) {
+            throw new IllegalArgumentException(reference + " is not a valid Reference.");
+        }
+        Pattern p = Pattern.compile("(\\d+)");
+        Matcher m = p.matcher(reference);
+        if (m.matches()) {
+            String rowPart = m.group(1);
+            return Integer.parseInt(rowPart);
+        } else {
+            throw new IllegalStateException("Valid reference didn't match any numbers");
+        }
+
+    }
+
+    /**
+     * Get the column number of the reference string.
+     * @param reference the string reference to a cell in the form "$A1" for first column first row
+     * @throws IllegalArgumentException if the string is not a reference
+     * @return The 1 indexed column number
      */
     static int column(String reference) {
         if (!isValidRef(reference)) {
             throw new IllegalArgumentException(reference + " is not a valid Reference.");
         }
-        return Integer.parseInt(reference.split("\\$\\w*/")[1]);
-        
+        Pattern p = Pattern.compile("([a-zA-Z]+)");
+        Matcher m = p.matcher(reference);
+        if (m.matches()) {
+            // From $AbC1 gets ['A', 'B', 'C']
+            char[] columnPart = m.group(1).toUpperCase().toCharArray();
+            int column = 0;
+            int multiplier = 1;
+            // loop in reverse order from least to most significant letter
+            for (int i = columnPart.length - 1; i >= 0; --i) {
+                // convert ascii letters to their alphabet position, so a=1, z=26
+                int val = (int) columnPart[i] - 64;
+                column += multiplier * val;
+                multiplier *= 26;
+            }
+            return column;
+        } else {
+            throw new IllegalStateException("How did this happen");
+        }
     }
-
-    // Get the cell
-
 
 
 
