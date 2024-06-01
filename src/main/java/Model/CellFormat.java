b7ccabaa006bf1a696778edd87aea9704ead2d23
NikitaClark <nikitaclark296@gmail.com>
2024-05-31 22:44:08 -0400

    using table view and table column to form a cells look

--- src/main/java/Model/CellFormat.java
+++ src/main/java/Model/CellFormat.java
@@ -1,8 +1,22 @@
 package Model;
 
 /**
- * Class containing information about the formatting of the cell
+ * Class representing a basic cell in a spreadsheet with only size information.
  */
 public class CellFormat {
+    private int width; // Width of the cell in pixels
+    private int height; // Height of the cell in pixels
 
+    public CellFormat(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
--- src/main/java/com/example/huskysheet/HelloApplication.java
+++ src/main/java/com/example/huskysheet/HelloApplication.java
@@ -2,24 +2,22 @@ package com.example.huskysheet;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
         stage.setTitle("HuskySheet");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
-
-
