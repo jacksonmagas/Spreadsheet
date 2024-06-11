d38c30cf44895784993f7034e3f30d1fc992ae6d
NikitaClark <nikitaclark296@gmail.com>
2024-06-10 22:38:15 -0400

    up

--- src/main/java/com/example/huskysheet/controller/HelloController.java
+++ src/main/java/com/example/huskysheet/controller/HelloController.java
@@ -3,31 +3,17 @@ package com.example.huskysheet.controller;
 import com.example.huskysheet.client.Model.ICell;
 import com.example.huskysheet.client.Model.Spreadsheet;
 
-import com.example.huskysheet.client.Utils.Conversions;
 import com.example.huskysheet.client.Utils.SpreadsheetSliceView;
 import com.example.huskysheet.client.Utils.SpreadsheetSliceView.Direction;
 import java.util.List;
-import javafx.application.Platform;
-import javafx.beans.Observable;
-import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.util.Callback;
-import javafx.util.Pair;
 import javafx.util.converter.DefaultStringConverter;
 
 import java.net.URL;
@@ -35,27 +21,19 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
     @FXML
-    private TableView<SpreadsheetSliceView> table;
-    @FXML
-    private TableColumn<SpreadsheetSliceView, String> rowNumbers;
-    @FXML
-    private Button newColumnButton;
-    @FXML
-    private Button newRowButton;
+    private TableView<List<ICell>> table;
+
 
     private String userName;
     private String password;
     private String url;
 
     // Define the number of rows and columns
-    private static final int INITIAL_ROW_NUM = 8;
-    private static final int INITIAL_COLUMN_NUM = 8;
-    private int numRows = 0;
-    private int numCols = 0;
-
+    private static final int ROWS = 8;
+    private static final int COLUMNS = 8;
     private Spreadsheet spreadsheet;
 
-    ObservableList<SpreadsheetSliceView> list = FXCollections.observableArrayList();
+    ObservableList<List<ICell>> list = FXCollections.observableArrayList();
 
     public void setUserName(String userName) {
         this.userName = userName;
@@ -72,77 +50,21 @@ public class HelloController implements Initializable {
 
         table.setEditable(true);
 
-        String headerStyle = "-fx-background-color: -fx-body-color; -fx-font-weight: bold; -fx-text-alignment: center;";
-
         table.getSelectionModel().setCellSelectionEnabled(true);
-        Callback<TableColumn<SpreadsheetSliceView, String>, TableCell<SpreadsheetSliceView, String>> rowNumFactory =
-            (tv) -> new TableCell<>() {
-                @Override
-                protected void updateItem(String item, boolean empty) {
-                    super.updateItem(item, empty);
-                    if (empty) {
-                        setText("");
-                        setStyle(headerStyle);
-                    } else {
-                        setText(item);
-                        setStyle(headerStyle);
-                    }
-                    // Prevent cell from being selectable
-                    setMouseTransparent(true);
-                    setFocusTraversable(false);
-                }
-            };
-        // Add a listener to the selection model
-        table.getSelectionModel().getSelectedCells().addListener(
-            new ListChangeListener<>() {
-                @Override
-                public void onChanged(Change<? extends TablePosition> c) {
-                    if (c.next()
-                        && !c.getAddedSubList().isEmpty()
-                        && c.getAddedSubList().getFirst().getColumn() == 0) {
-                        int row = c.getAddedSubList().getFirst().getRow();
-                        int col = c.getAddedSubList().getFirst().getColumn() + 1;
-                        Platform.runLater(() -> table.getSelectionModel().clearSelection());
-                        Platform.runLater(() -> table.getSelectionModel().select(row, table.getColumns().get(col)));
-                    }
-                }
-            });
-        rowNumbers.setCellFactory(rowNumFactory);
-        rowNumbers.setCellValueFactory(cellData ->
-            new SimpleStringProperty(Integer.toString(cellData.getValue().getRowColNumber())));
-        rowNumbers.setPrefWidth(50);
-        rowNumbers.setResizable(false);
-        rowNumbers.setEditable(false);
-        rowNumbers.setSortable(false);
-        table.getColumns().add(rowNumbers);
 
         // Create the table columns dynamically
-        for (int i = 1; i <= Math.max(spreadsheet.numColumns(), INITIAL_COLUMN_NUM); i++) {
-            newColumn();
+        for (int i = 1; i <= COLUMNS; i++) {
+            TableColumn<List<ICell>, String> column = new TableColumn<>("Column " + i);
+            setupColumn(column, i);
+            table.getColumns().add(column);
         }
 
-        // Populate the table with rows of the spreadsheet
-        for (int row = 1; row <= Math.max(spreadsheet.numRows(), INITIAL_ROW_NUM); row++) {
-            newRow();
+        // Populate the table with CellFormat objects
+        for (int row = 1; row <= ROWS; row++) {
+            list.add(new SpreadsheetSliceView(spreadsheet, Direction.row, row));
         }
-
-        newColumnButton.setOnAction(event -> {newColumn();});
-        newRowButton.setOnAction(event -> {newRow();});
 
         table.setItems(list);
-    }
-
-    private void newRow() {
-        list.add(new SpreadsheetSliceView(spreadsheet, Direction.row, numRows + 1));
-        ++numRows;
-    }
-
-    private void newColumn() {
-        TableColumn<SpreadsheetSliceView, String> column = new TableColumn<>(Conversions.columnToString(numCols + 1));
-        column.setPrefWidth(100);
-        setupColumn(column, numCols + 1);
-        table.getColumns().add(column);
-        ++numCols;
     }
 
     /**
@@ -153,7 +75,7 @@ public class HelloController implements Initializable {
      * @param column the column to set up
      * @param columnNumber the column number
      */
-    private void setupColumn(TableColumn<SpreadsheetSliceView, String> column, int columnNumber) {
+    private void setupColumn(TableColumn<List<ICell>, String> column, int columnNumber) {
         column.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(columnNumber).getData()));
         column.setEditable(true);
         column.setCellFactory(TextFieldTableCell.forTableColumn(new DefaultStringConverter()));
